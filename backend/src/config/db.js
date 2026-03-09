4610bbbd571e5eaf91ee361a8fca1ccb31af1de3
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-03-09 01:57:21 -0300

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,14 +1,14 @@
-import mysql from "mysql2/promise";
+import pg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
+const { Pool } = pg;
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: 3306
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL, // usar DATABASE_URL do Supabase
+  ssl: {
+    rejectUnauthorized: false, // necessário para Supabase
+  },
 });
 
 export default pool;
