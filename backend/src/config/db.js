80e0e3872e3368bb9743a9cd0226e3a56a53af08
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-03-09 02:57:46 -0300

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -8,7 +8,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT), // converter para número
+  port: Number(process.env.DB_PORT),
   ssl: {
     rejectUnauthorized: false, // ESSENCIAL para Supabase
   },
@@ -20,7 +20,7 @@ async function testConnection() {
     console.log("✅ Banco conectado com sucesso!");
     client.release();
   } catch (err) {
-    console.error("❌ Erro ao conectar no banco:", err);
+    console.error("❌ Erro ao conectar no banco:", err.message);
   }
 }
 
