18f3db760d978dcc123d2dfd2532e04dedc4a1e9
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-03-09 02:33:14 -0300

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,17 @@
 import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: 5432,
+  port: process.env.DB_PORT, // use 5432 do .env
+  ssl: {
+    rejectUnauthorized: false, // ESSENCIAL para Supabase
+  },
 });
 
 async function testConnection() {
