438dddbf1de1bc782601f87889882926ebd3e870
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-03-10 02:37:54 -0300

    Update user.controller.js

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,11 +1,11 @@
 import db from "../config/db.js";
 
 export const listar = async(req, res) => {
-	const [rows] = await db.query("SELECT id, nome, email, acesso FROM usuarios");
-	res.json(rows)
+	const result = await db.query("SELECT id, nome, email, acesso FROM usuarios");
+	res.json(result.rows);
 };
 
 export const deletar = async(req, res) => {
-	await db.query("DELETE FROM usuarios WHERE id=?",[req.params.id]);
+	await db.query("DELETE FROM usuarios WHERE id=$1",[req.params.id]);
 	res.json({msg:"Usuário excluido com sucesso!!!"})
 }
