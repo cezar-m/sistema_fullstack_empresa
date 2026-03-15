1afd81bb4d5c79f25a418f9b44c1b857e51e8c49
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-03-14 22:59:33 -0300

    Update Usuarios.jsx

--- frontend/src/pages/Usuarios.jsx
+++ frontend/src/pages/Usuarios.jsx
@@ -2,17 +2,6 @@ import { useEffect, useState } from "react";
 import api from "../api/api";
 import DashboardLayout from "../layouts/DashboardLayout";
 
-const api = axios.create({
-	baseURL: "http://localhost:5000/api",
-});
-
-// envia token automaticamente
-api.interceptors.request.use((config) => {
-	const token = localStorage.getItem("token");
-	if(token) config.headers.Authorization = `Bearer ${token}`;
-	return config;
-});
-
 export default function Usuarios() {
 	const [usuarios, setUsuarios] = useState([]);
 	const [mensagem, setMensagem] = useState("");
