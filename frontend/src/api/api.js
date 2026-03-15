7188e5886ceef5fc89b07c770e91263dbbb8af1e
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-03-15 03:39:21 -0300

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,21 +1,18 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
-  console.log("API URL:", import.meta.env.VITE_API_URL);
+  baseURL: import.meta.env.VITE_API_URL + "/api",
 });
 
-// Interceptor de token
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-    if (!(config.data instanceof FormData)) {
-      config.headers["Content-Type"] = "application/json";
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
 
 export default api;
+
