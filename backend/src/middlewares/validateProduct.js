d0c56335e7c8fb7b0cc7e5ec0c85f8d2b4631199
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-03-16 00:17:08 -0300

    Update validateProduct.js

--- backend/src/middlewares/validateProduct.js
+++ backend/src/middlewares/validateProduct.js
@@ -1,19 +1,16 @@
 import db from "../config/db.js";
 
 export default async function validateProduct(req, res, next) {
-	try {
-		const { id } = req.params;
-		const result = await db.query(
-			"SELECT * FROM produtos WHERE id=$1",
-			[id]
-		);
-		if(result.rows.length === 0){
-			return res.status(404).json({ erro: "Produto não encontrado" });
-		}
-		req.produto = result.rows[0];
-		next();
-	} catch(err) {
-		console.error(err);
-		res.status(500).json({ erro: "Erro no middleware de validação" });
-	}
+  try {
+    const { id } = req.params;
+    const result = await db.query("SELECT * FROM produtos WHERE id=$1", [id]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({ erro: "Produto não encontrado" });
+    }
+    req.produto = result.rows[0];
+    next();
+  } catch (err) {
+    console.error("Erro validateProduct:", err);
+    res.status(500).json({ erro: "Erro no middleware de validação" });
+  }
 }
