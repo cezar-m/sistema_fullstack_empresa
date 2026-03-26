20bd2d1de9ba19c7e212e594a12fd3d3372eed0f
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-03-25 22:38:37 -0300

    Update pagamentos.routes.js

--- backend/src/routes/pagamentos.routes.js
+++ backend/src/routes/pagamentos.routes.js
@@ -8,6 +8,6 @@ router.post("/", auth, controller.criarPagamento);
 router.put("/pago/:id", auth, controller.marcarComoPago); // rota para alterar status
 router.get("/", auth, controller.listarPagamentosPorId);
 router.get("/:id/parcelas", auth, controller.listarParcelasPorPagamento);
-router.put("/parcelas/:id", auth, controller.atualizarParcela);
+router.put("/parcelas/:id", auth, controller.atualizarParcelas);
 
 export default router; 
